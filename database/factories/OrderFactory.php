6b292d465894a2c237eb3a724070d2127ccc98db
dassepascal <pascaldasse56@gmail.com>
2023-10-19 17:14:11 +0200

    factories

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -17,7 +17,8 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'order_number'=>uniqid(),
+           
         ];
     }
 }
