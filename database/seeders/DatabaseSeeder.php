1f56d3a9812cc0fa83e2cca4cb5d019cebede816
dassepascal <pascaldasse56@gmail.com>
2023-10-19 17:16:41 +0200

    DatabaseSeeder copie à partir du git

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Order;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()
+            ->count(10)
+            ->has(
+                Order::factory()
+                    ->count(3)
+                    ->hasAttached(
+                        Product::factory()->count(5),
+                        ['price' => rand(100, 500), 'quantity' => rand(1, 3)]
+                    )
+            )
+            ->create();
     }
 }
