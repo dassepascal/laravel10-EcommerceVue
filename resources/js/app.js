9bc5a808a736299e4925a88449a4876f2c16ad74
dassepascal <pascaldasse56@gmail.com>
2023-10-28 14:43:30 +0200

    ajout vue dans tailwind + creation de NavbarCart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
-import AddToCart from './components/AddToCart.vue';
+import AddToCart from './components/AddToCart';
+import NavbarCart from './components/NavbarCart';
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
 const app = createApp();
-app.component('AddToCart', AddToCart)
+app.component('AddToCart', AddToCart);
+app.component('NavbarCart',NavbarCart);
 app.mount('#app');
